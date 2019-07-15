bdd337ba1a3ff12b49a87a68c4c87e2e8cbe6a74
Julianna609 <chebotar609@gmail.com>
2019-07-15 13:44:55 +0300

    style/js: review page

--- src/js/app.js
+++ src/js/app.js
@@ -28,5 +28,57 @@ import { log } from './utils';
     $('.js-contact-widget').click(function () {
       $(this).toggleClass('opened');
     });
+
+    // product descriptions accordion
+
+    const initDescriptionAccodrion = () => {
+      const showChar = $('window').width() > 991 ? 320 : 190;// How many characters are shown by default
+      $('.js-review-description').each(function () {
+        const content = $(this).html();
+
+        if (content.length > showChar) {
+          const c = content.substr(0, showChar);
+          const h = content.substr(showChar, content.length - showChar);
+
+          const html = `${c} <span class="morecontent"><span> ${h} </span><a href="#" class="show-more-button js-see-more">Show more . . .</a></span>`;
+
+          $(this).html(html);
+        }
+      });
+    }; initDescriptionAccodrion();
+
+    $('.js-see-more').click(function (e) {
+      e.preventDefault();
+      const $this = $(this);
+      const moretext = `Show more . . .`;
+      const lesstext = `Show less`;
+      if ($this.hasClass('less')) {
+        $this.removeClass('less');
+        $this.html(moretext);
+      } else {
+        $this.addClass('less');
+        $this.html(lesstext);
+      };
+      $this.parent().prev().css('display', 'inline');
+      $this.prev().toggle();
+    });
+
+    // reviews load
+    function reviewsLoad () {
+      if ($('.reviews .review-item').length > 3) {
+        $('.reviews .container').append(`<a href="/" class="review__see-more js-more-review">Показать еще отзывы</a>`);
+
+        $('.reviews .review-item').slice(0, 3).css('display', 'flex');
+        $('.js-more-review').on('click', function (e) {
+          e.preventDefault();
+          $('div:hidden').slice(0, 4).css('display', 'flex');
+          if ($('div:hidden').length === 0) {
+            $('#load').fadeOut('slow');
+          }
+        });
+      } else {
+        $('.reviews .review-item').css('display', 'flex');
+      }
+    } reviewsLoad();
   });
 })(window.jQuery);
